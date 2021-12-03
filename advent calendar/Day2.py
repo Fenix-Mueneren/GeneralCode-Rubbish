e3c6d984def57cb35853cbf4f52cdcd9cd5d5be5
Fenix Mueneren <vrg.phoenix@gmail.com>
2021-12-03 13:35:15 +0100

    Day2 Challenge files

---
+++ advent calendar/Day2.py
@@ -0,0 +1,39 @@
+## Author Fenix Mueneren
+
+
+input_file = open("advent calendar/inputs/day2input.txt", "r")
+content = input_file.read()
+
+plottedcourse = content.split("\n")
+input_file.close()
+horizontal = 0 
+depth = 0
+
+
+for step in plottedcourse :
+    course = step.split(" ")
+    if (course[0]) == "forward":
+        horizontal += int(course[1])
+    elif (course[0]) == "up":
+        depth -= int(course[1])
+    elif (course[0]) == "down":
+        depth += int(course[1])
+
+print(depth * horizontal)
+
+horizontal = 0 
+depth = 0
+aim = 0
+
+for step in plottedcourse :
+    course = step.split(" ")
+    if (course[0]) == "forward":
+        horizontal += int(course[1])
+        depth += aim*int(course[1])
+    elif (course[0]) == "up":
+        aim -= int(course[1])
+    elif (course[0]) == "down":
+        aim += int(course[1])
+        
+
+print(depth * horizontal)

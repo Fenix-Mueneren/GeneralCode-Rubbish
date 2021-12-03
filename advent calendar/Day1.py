b0e10b56b2a5e1646ddc699397da03e9af76ef0c
Fenix Mueneren <vrg.phoenix@gmail.com>
2021-12-03 13:01:31 +0100

    Final Day1 working!

--- advent calendar/Day1.py
+++ advent calendar/Day1.py
@@ -10,7 +10,17 @@ input_file.close()
 
 for index, value in enumerate(values):
     if index-1 >= 0 :
-        if value > values[index-1]:
+        if int(value) > int(values[index-1]):
                 largemeasurements += 1
     
 print(largemeasurements)
+
+largerwindows = 0
+for index, value in enumerate(values):
+    if index < len(values)-3 :
+        windowA = int(values[index]) + int(values[index+1]) + int(values[index+2])
+        windowB = int(values[index+1]) + int(values[index+2]) + int(values[index+3])
+        if windowB > windowA:
+                largerwindows += 1
+    
+print(largerwindows)
